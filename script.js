7899bfe0596db099f75464946664d5921e80f02b
fastflowrapper <deniscoolestintheworld@gmail.com>
2020-06-06 23:25:29 +0300

    lesson02-01

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 let money, income, addExpences, deposit, mission, period;
 
-alert("Hello");
-console.log('Hello again :) ');
+money = 55555;
+income = "selling cookies";
+addExpences = "Chocolate, Flour, Sugar";
+deposit = true;
+mission = 1000000;
+period = 12;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpences.length);
+
+console.log("Период равен " + period + " месяцев");
+
+console.log("Цель заработать " + mission + " рублей");
+
+console.log( addExpences.toLowerCase().split(', ') );
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
