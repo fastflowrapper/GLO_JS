ac18341daf1fc1a8b8599d15a2c359247444cd47
fastflowrapper <deniscoolestintheworld@gmail.com>
2020-06-08 23:03:02 +0300

    lesson03-01

--- script.js
+++ script.js
@@ -21,3 +21,30 @@ console.log( addExpences.toLowerCase().split(', ') );
 
 let budgetDay = money / 30;
 console.log('budgetDay: ', budgetDay);
+
+//lesson03
+
+money = parseInt( +prompt('Ваш месячный доход?') );
+
+addExpences = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+let expences1 = prompt('Введите обязательную статью расходов');
+let amount1 = prompt('Во сколько это обойдется?');
+let expences2 = prompt('Введите обязательную статью расходов');
+let amount2 = prompt('Во сколько это обойдется?');
+
+let budgetMonth = money - ( +amount1) - (+amount2);
+console.log('Бюджет на месяц: ', budgetMonth);
+
+console.log('Цель будет достигнута за ' + Math.ceil( mission / budgetMonth ) + ' месяцев' );
+
+budgetDay = Math.floor(budgetMonth / 30);
+console.log('Бюджет на день: ', budgetDay);
+
+(budgetDay >= 1200) ? console.log('У вас высокий уровень дохода')
+    : (budgetDay >= 600 && budgetDay < 1200) ? console.log('У вас средний уровень дохода')
+    : (budgetDay >= 0 && budgetDay < 600) ? console.log('У вас низкий уровень дохода')
+    : console.log('Что-то пошло не так');
+
